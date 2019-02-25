37b4c28cf29b0decc209104bf9f54ef452d59b5c
Htet Naing <htet.naing91@gmail.com>
2019-02-26 02:08:27 +0800

    Initial commit

---
+++ Accelerated_Cpp_Exercise_1/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  Playground
+//
+//  Created by HTET NAING on 2/25/19.
+//  Copyright © 2019 com.Javelin. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
