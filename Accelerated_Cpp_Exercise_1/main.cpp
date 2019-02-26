a025f3d721d35d0de5acec1e4d39bd37d19dd6a2
Htet Naing <htet.naing91@gmail.com>
2019-02-26 14:59:43 +0800

    Add main.cpp

---
+++ Accelerated_Cpp_Exercise_1/main.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include "grade.hpp"
+#include "Student_info.hpp"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::domain_error;
+using std::max;
+using std::setprecision;
+using std::sort;
+using std::streamsize;
+using std::string;
+using std::vector;
+
+int main()
+{
+    vector<Student_info> students;
+    vector<double> homework;
+    Student_info record;
+    int maxlen = 0;   // the length of the longest name
+
+    // read and store all the student's data.
+    // Invariant:   students contain all the student records read so far
+    //              maxlen contains the length of the longest name in students
+    
+    while (true)
+    {
+        // find the length of longest name
+        string name;
+        double midterm_score;
+        double final_score;
+        double x;
+        cout << "Please enter student name: ";
+        cin >> name;
+        if (name[0] == '#') {
+            break;
+        }
+        cin.get();
+        cout << "Please enter midterm score: ";
+        cin >> midterm_score;
+        cout << "Please enter final score: ";
+        cin >> final_score;
+        cout << "Please enter homework score: ";
+        while(cin >> x) {
+            homework.push_back(x);
+        }
+        record = {name, midterm_score, final_score, homework};
+        maxlen = record.name.size();
+        students.push_back(record);
+        cin.clear();
+    }
+
+    // alphabetize the student records
+    sort(students.begin(), students.end(), compare);
+
+    // write the names and grades
+    for (vector<Student_info>::size_type i = 0;
+         i != students.size(); ++i)
+    {
+        //write the name, padded on teh right to maxlen + 1 characters
+        cout << students[i].name
+             << string(maxlen + 1 - students[i].name.size(), ' ');
+
+         //compute and write the grade
+        try
+        {
+            double final_grade = grade(students[i]);
+            streamsize prec = cout.precision();
+            cout << setprecision(3) << final_grade
+                 << setprecision(prec);
+        }
+        catch (domain_error e)
+        {
+            cout << e.what();
+        }
+        cout << endl;
+    }
+    return 0;
+}
